b066d7d02a4cae0ad5200b0fce7907da57855721
Zhuo Nengwen <soarowl@yeah.net>
2024-02-15 10:51:25 +0800

    Command line processing added.

--- source/app.d
+++ source/app.d
@@ -1,6 +1,17 @@
-import std.stdio;
+import argparse;
 
-void main()
+@(Command("dcu2pas").Description("Decompile dcu(Delphi Compiled Unit) to pas.")
+        .Epilog(() => "Best wishes for your happiness and success"))
+struct Config
 {
-	writeln("Edit source/app.d to start your project.");
+    @(PositionalArgument(0).Description(() => "File to decompile"))
+    string[] file;
 }
+
+mixin CLI!Config.main!((args) {
+    // do whatever you need
+    import std.stdio : writeln;
+
+    args.writeln;
+    return 0;
+});
